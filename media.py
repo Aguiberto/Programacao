dfd4ab2d876fa2fafff70cc8ee44dbdde0f4c8af
Aguiberto <aguibertocandido@gmail.com>
2025-06-09 15:45:02 -0300

    atualização

--- media.py
+++ media.py
@@ -1,8 +1,18 @@
-lista = [5,1,2,3,4,5]
+# lista = [5,1,3,5,7,9]
 
-total = len(lista)
-soma = sum(lista)
+# total = len(lista)
+# soma = sum(lista)
 
-media = soma // total
+# media = soma // total
 
-print(media)
+# print(media)
+
+def media_lista(lista):
+    lista = [5,1,2,3,4,5]
+
+    total = len(lista)
+    soma = sum(lista)
+
+    media = soma // total
+
+    return media
