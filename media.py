52b3c2aa54dae824c8c7671e370ae8655b570f8d
Aguiberto <aguibertocandido@gmail.com>
2025-06-09 15:37:08 -0300

    media

---
+++ media.py
@@ -0,0 +1,8 @@
+lista = [5,1,2,3,4,5]
+
+total = len(lista)
+soma = sum(lista)
+
+media = soma // total
+
+print(media)
--- troca.py
+++ troca.py
@@ -1,36 +1,35 @@
-# lista = [2,4,6,8,1,3,5,7]
+lista = [91,58,-41,85,-81,87,9,10,-49,39]
 
-# lista2 = lista.copy()
-# lista3 = lista.copy()
-# menor = lista.index(min(lista))
-
-
-# if lista[0] > lista[menor]:
-#     lista2 [0] = lista[menor] 
-#     lista3[0] = lista2[0]
-#     lista3[menor] = lista[0]
-
-#     print('1')
-#     print(lista3)
-# else:
-#     print('0')
-
-# def lista_troca_menor_primeiro(lista):
-
-lista = [2,4,6,8,1,3,5,7]
 lista2 = lista.copy()
 lista3 = lista.copy()
 menor = lista.index(min(lista))
 
-    
+
 if lista[0] > lista[menor]:
     lista2 [0] = lista[menor] 
     lista3[0] = lista2[0]
     lista3[menor] = lista[0]
 
-    #  return 1, lista3
     print('1')
     print(lista3)
 else:
-    # return 0
     print('0')
+
+# def lista_troca_menor_primeiro(lista):
+
+#     lista = [2,4,6,8,1,3,5,7]
+#     lista2 = lista.copy()
+#     lista3 = lista.copy()
+#     menor = lista.index(min(lista))
+
+    
+#     if lista[0] > lista[menor]:
+#         lista2 [0] = lista[menor] 
+#         lista3[0] = lista2[0]
+#         lista3[menor] = lista[0]
+
+#         return 1
+    
+#     else:
+#         return 0
+   
